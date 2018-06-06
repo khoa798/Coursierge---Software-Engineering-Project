6259cfd5ee216ad53b386e958afe6c28a24a7ea2
Khoa Hoang <khoahoang3k@gmail.com>
2018-06-05 18:50:21 -0700

    Set up basic framework to display Professor Question results as a graph.

---
+++ app/src/main/java/com/jhacks/vrclassroom/Chart.java
@@ -0,0 +1,82 @@
+package com.jhacks.vrclassroom;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.github.mikephil.charting.data.BarEntry;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+public class Chart extends AppCompatActivity {
+    String textDebug = "[DEBUG]";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chart);
+
+
+        final String question = getIntent().getStringExtra("Text");
+
+        final ArrayList<String> choices = new ArrayList<>();
+        DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference().child("questions").child(question);
+        mDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                    choices.add(ds.getValue().toString());
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.d(textDebug, "The read failed.");
+            }
+
+
+
+        });
+
+
+        final DatabaseReference mScoresDatabase = FirebaseDatabase.getInstance().getReference().child("question-scores").child(question);
+        mScoresDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists())
+                {
+                    QuestionScores currQuestion;
+                    currQuestion = dataSnapshot.getValue(QuestionScores.class);
+                    ArrayList<BarEntry> entries = new ArrayList<>();
+                    entries.add(currQuestion.)
+
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.d(textDebug, "The read failed.");
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+}
--- app/src/main/java/com/jhacks/vrclassroom/ViewResponses.java
+++ app/src/main/java/com/jhacks/vrclassroom/ViewResponses.java
@@ -1,14 +1,68 @@
 package com.jhacks.vrclassroom;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class ViewResponses extends AppCompatActivity {
+    String textDebug = "[DEBUG]";
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_responses);
+        DatabaseReference mDatabase;
+        final ArrayList<String> arrli = new ArrayList<String>();
 
+        mLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("questions");
+        mDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                    //System.out.println("CURRENT KEY: " + ds.getKey());
+                    arrli.add(ds.getKey());
+                }
+
+
+                RecycleViewClickListener listener = new RecycleViewClickListener() {
+                    @Override
+                    public void onClick(View view, int position, String text) {
+                        Intent intent = new Intent(ViewResponses.this, Chart.class);
+                        intent.putExtra("Text", text);
+                        intent.putExtra("Position", position);
+                        startActivity(intent);
+
+                    }
+                };
+
+                mRecyclerView = findViewById(R.id.questionView);
+                mAdapter = new RVAdapter(arrli, listener);
+                mRecyclerView.setHasFixedSize(true);
+                mRecyclerView.setLayoutManager(mLayoutManager);
+                mRecyclerView.setAdapter(mAdapter);
+                //mAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.d(textDebug, "The read failed.");
+            }
+        });
     }
 }
